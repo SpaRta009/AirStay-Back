c00804629915ddbdbbba501484dc9d24762acce9
Mohamed <yousreus11@gmail.com>
2026-05-06 17:35:35 +0100

    fix: add 0000_postgis migration to resolve conflict with PostGIS

--- backEnd/migrations/0000_postgis.py
+++ backEnd/migrations/0000_postgis.py
@@ -1,10 +1,8 @@
 from django.db import migrations
 
+
 class Migration(migrations.Migration):
 
     initial = True
     dependencies = []
-
-    operations = [
-        migrations.RunSQL("CREATE EXTENSION IF NOT EXISTS postgis;"),
-    ]
+    operations = []
