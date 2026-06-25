f795515631bbd3d38170f279aa924ab92bc3a1b2
Mohamed <yousreus11@gmail.com>
2026-06-25 15:18:10 +0100

    Configuration Supabase terminée et ajout du gitignore

--- backEnd/apps.py
+++ backEnd/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
-
 class BackendConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backEnd'
+
+    def ready(self):
+        import backEnd.signals
